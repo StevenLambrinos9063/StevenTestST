aabf4fcbf864fe10ef298ff68bc217139987cffe
stevenlambrinos <Stevielam442@gmail.com>
2022-06-16 18:03:17 -0400

    Initial commit

---
+++ app/src/main/java/steven/lambrinos/n01429063/s/MainActivity.java
@@ -0,0 +1,14 @@
+package steven.lambrinos.n01429063.s;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
